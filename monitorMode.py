a180cfb2cbf79114e9938bbda926f05f8b383f09
TomLatin <tomlatin52@gmail.com>
2020-05-10 11:53:22 +0300

    just start

---
+++ monitorMode.py
@@ -0,0 +1,6 @@
+import platform  # provides system information like on which the program is being currently executed
+import os  # for interacting with the operating system
+import psutil  # used to access system details and process utilities,For Windows service library
+import subprocess  # For Linux services
+import datetime
+import time
